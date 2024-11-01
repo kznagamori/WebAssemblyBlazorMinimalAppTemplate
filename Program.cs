bca29d432c06c09225a3e68dac4dc6a0f5861aff
kznagamori <kazuyuki.nagamori@gmail.com>
2024-11-01 21:11:49 +0900

    Routes.razorの配置を修正

--- Program.cs
+++ Program.cs
@@ -1,9 +1,10 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using ___PROJECTNAME___;
+using ___PROJECTNAME___.Components;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
-builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<Routes>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
